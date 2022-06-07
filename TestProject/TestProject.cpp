7b348c1a2f4ea5807a611ee7132cff1fdcb4e4a8
FiluBlack <38252422+FiluBlack@users.noreply.github.com>
2022-06-07 18:27:17 +0200

    Added TestProject for unit tests.

---
+++ TestProject/TestProject.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestProject
+{
+	TEST_CLASS(TestProject)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
